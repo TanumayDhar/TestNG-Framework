5b24a32e2a6b15d8568e06db3d043a925550f7a2
Tanumay Dhar <tanna.dhar@gmail.com>
2020-10-01 18:35:37 +0530

    updated-prioritastn/Ignore/timeout/enabled/dependson/priority

--- src/test/java/Annotations/TestNGDemoTestCases3.java
+++ src/test/java/Annotations/TestNGDemoTestCases3.java
@@ -3,44 +3,52 @@ package Annotations;
 import org.testng.annotations.Test;
 
 public class TestNGDemoTestCases3 {
-	
+
 	//Prioritisation of test cases
-	@Test
+	//Lower priorities will be scheduled first
+
+
+	@Test(priority=1)
 	public void banking()
 	{
-		
+
 		System.out.println("banking");
-		
+
 	}
+
+	//Dependency test
+	//Depends on method
 	//if "banking " test case fail also "Add customer" will execute due to attribute alwaysRun=true
 	@Test(dependsOnMethods= {"banking"}, alwaysRun=true)
 	public void AddCustomer()
 	{
 		System.out.println("AddCustomer");
-			
+
 	}
-	
+
+	//Ignore test
+
 	@Test(enabled=false)
 	public void createAccount()
 	{
 		System.out.println("createAccount");
-		
-		
+
+
 	}
-	
+
 	@Test(enabled=true)
 	public void createNEWAccount()
 	{
 		System.out.println("createAccountNew");
-			
+
 	}
 	//time should take 1 sec , otherwise fail
 	@Test(timeOut=1000)
 	public void timeToexecute()
 	{
 		System.out.println("timeToexecute is 1 sec");
-			
+
 	}
-	
-	
+
+
 }
